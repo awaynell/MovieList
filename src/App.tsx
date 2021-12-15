3f08e86c119d78a426261db6f83023d4dbaf21e3
awaynell <dreamerfool@yandex.ru>
2021-12-15 16:00:29 +0300

    add selectors.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Button } from "@mui/material";
 import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 import { testAction } from "./redux/actionCreators";
+import { textSelector } from "./redux/selectors";
 
 export const App = () => {
   const dispatch = useDispatch();
 
-  const { text } = useSelector((state: RootStateOrAny) => state);
+  const text = useSelector(textSelector);
+  console.log("text: ", text);
 
   return (
     <>
