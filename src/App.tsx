15a158b1dfcc55c62128f918aa20ee87bb525e5a
Ilya Zakharov <i.zaharov@id-east.ru>
2022-04-25 20:11:40 +0300

    delete some info, unfortunately TMDB banned access from Russia (maybe switch to another API soon)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import "./App.scss";
-import Header from "./components/Header/Header";
-import AppRouter from "./components/AppRouter/AppRouter";
-import LoginModal from "./components/MoviesPage/LoginModal/LoginModal";
+import './App.scss';
+import AppRouter from './components/AppRouter/AppRouter';
 
 const App = () => {
   return (
